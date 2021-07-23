253273592f1d88ca720e89762be8e70160e6c5e7
sharvin18 <dedhiasharvin@gmail.com>
2021-07-23 12:16:47 +0530

    Initial commit

---
+++ src/main/java/com/sharvin/Coronavirustracker/services/CoronavirusServices.java
@@ -0,0 +1,28 @@
+package com.sharvin.Coronavirustracker.services;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.*;
+
+@Service
+public class CoronavirusServices {
+
+    private static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    @PostConstruct
+    public void fetchData() throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(DATA_URL))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+    }
+}
